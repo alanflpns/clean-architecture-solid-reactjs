aba043d9765296f4ceff049ebaec5c65e1945027
alanflpns <alanflpns@gmail.com>
2022-04-22 16:52:35 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,12 +2,13 @@ import faker from "@faker-js/faker";
 
 import { HttpPostClientSpy } from "../../test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
-import { mockAuthentication } from "../../../domain/test/mock-authentication";
+import { mockAuthentication } from "../../../domain/test/mock-account";
 import { InvalidCredentialsError } from "../../../domain/errors/invalid-credentials-error";
 import { HttpStatusCode } from "../../protocols/http/http-response";
 import { UnexpectedError } from "../../../domain/errors/unexpected-error";
 import { AuthenticationParams } from "../../../domain/usecases/authentication";
 import { AccountModel } from "../../../domain/models/account-model";
+import { mockAccountModel } from "../../../domain/test/mock-account";
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -77,5 +78,16 @@ describe("RemoteAuthentication", () => {
     };
     const promise = sut.auth(mockAuthentication());
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  it("Should return an AccountModel if HttpPostClient returns 200", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const account = await sut.auth(mockAuthentication());
+    expect(account).toEqual(httpResult);
   });
 });
