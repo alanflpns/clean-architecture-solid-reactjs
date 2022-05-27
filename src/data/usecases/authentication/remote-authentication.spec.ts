dacbafa1979e84a51b4923dc3ed24c94a8009e82
alanflpns <alanflpns@gmail.com>
2022-05-27 17:00:20 -0300

    refactor: move axios mock to a separate file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,4 +1,4 @@
-import faker from "@faker-js/faker";
+import faker from "faker";
 
 import { HttpPostClientSpy } from "../../test";
 import { RemoteAuthentication } from "./remote-authentication";
--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,26 +1,37 @@
 import { AxiosHttpClient } from "./axios-http-client";
+import { mockAxios } from "../../test";
 import axios from "axios";
-import faker from "@faker-js/faker";
-import { HttpPostParams } from "../../../data/protocols/http";
+import { mockPostRequest } from "../../../data/test";
 
 jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-// Factory
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient();
+type SutTypes = {
+  sut: AxiosHttpClient;
+  mockedAxios: jest.Mocked<typeof axios>;
 };
 
-const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement({}),
-});
+// Factory
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
+
+  return {
+    sut,
+    mockedAxios,
+  };
+};
 
 describe("AxiosHttpClient", () => {
   it("Should call axios with correct values", async () => {
     const request = mockPostRequest();
-    const sut = makeSut();
+    const { sut, mockedAxios } = makeSut();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  it("Should return the correct statusCode and body", () => {
+    const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockPostRequest());
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
   });
 });
