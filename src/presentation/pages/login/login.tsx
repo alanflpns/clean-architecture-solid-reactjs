acaea5b2d7fb791bde0b8c754067d127560ba295
alanflpns <alanflpns@gmail.com>
2022-05-28 20:11:16 -0300

    refactor: add index files with multiple imports

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,14 +1,10 @@
-import Footer from "../components/footer/footer";
-import FormStatus from "../components/form-status/form-status";
-import Input from "../components/input/input";
-import Header from "../components/login-header/login-header";
-import Spinner from "../components/spinner/spinner";
+import { LoginHeader, Input, FormStatus, Footer } from "../../components";
 import styles from "./login-styles.module.scss";
 
 function Login() {
   return (
     <div className={styles.login}>
-      <Header />
+      <LoginHeader />
       <form className={styles.form}>
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite seu e-mail" />
