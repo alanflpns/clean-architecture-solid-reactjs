77e8408d7f1f10dbe8b44458ada53027fad43a1f
alanflpns <alanflpns@gmail.com>
2022-05-28 22:27:54 -0300

    test: ensure login calls validation with correct passoword

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -56,11 +56,19 @@ describe("Login Component", () => {
     expect(passwordStatus.textContent).toBe("🔴");
   });
 
-  it("Shoul call Validation with correct values", () => {
+  it("Shoul call Validation with correct email", () => {
     const { sut, validationSpy } = makeSut();
 
     const emailInput = sut.getByTestId("email");
     fireEvent.input(emailInput, { target: { value: "any_email" } });
     expect(validationSpy.input).toEqual({ email: "any_email" });
+  });
+
+  it("Shoul call Validation with correct password", () => {
+    const { sut, validationSpy } = makeSut();
+
+    const passwordInput = sut.getByTestId("password");
+    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+    expect(validationSpy.input).toEqual({ password: "any_password" });
   });
 });
