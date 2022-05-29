2fd31f1e7ef36e3515b3d37599066b51c07bfae5
alanflpns <alanflpns@gmail.com>
2022-05-28 22:07:10 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,35 @@
-import { render, screen } from "@testing-library/react";
+/* eslint-disable testing-library/render-result-naming-convention */
+/* eslint-disable testing-library/prefer-screen-queries */
+import { render, RenderResult } from "@testing-library/react";
 import Login from "./login";
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+
+  return {
+    sut,
+  };
+};
 
 describe("Login Component", () => {
   it("Should start with initial state", () => {
-    render(<Login />);
+    const { sut } = makeSut();
 
-    const errorWrap = screen.getByTestId("error-wrap");
+    const errorWrap = sut.getByTestId("error-wrap");
     expect(errorWrap.childElementCount).toBe(0);
 
-    const submitButton = screen.getByTestId("submit") as HTMLButtonElement;
+    const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
 
-    const emailStatus = screen.getByTestId("email-status");
+    const emailStatus = sut.getByTestId("email-status");
     expect(emailStatus.title).toBe("Campo obrigatório");
     expect(emailStatus.textContent).toBe("🔴");
 
-    const passwordStatus = screen.getByTestId("password-status");
+    const passwordStatus = sut.getByTestId("password-status");
     expect(passwordStatus.title).toBe("Campo obrigatório");
     expect(passwordStatus.textContent).toBe("🔴");
   });
