80544eebef1f8b7343e624f4391f89a8c7b649d9
alanflpns <alanflpns@gmail.com>
2022-05-28 22:00:51 -0300

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,10 +4,19 @@ import Login from "./login";
 describe("Login Component", () => {
   it("Should start with initial state", () => {
     render(<Login />);
+
     const errorWrap = screen.getByTestId("error-wrap");
     expect(errorWrap.childElementCount).toBe(0);
 
     const submitButton = screen.getByTestId("submit") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
+
+    const emailStatus = screen.getByTestId("email-status");
+    expect(emailStatus.title).toBe("Campo obrigatório");
+    expect(emailStatus.textContent).toBe("🔴");
+
+    const passwordStatus = screen.getByTestId("password-status");
+    expect(passwordStatus.title).toBe("Campo obrigatório");
+    expect(passwordStatus.textContent).toBe("🔴");
   });
 });
