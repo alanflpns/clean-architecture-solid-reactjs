a877f9c8ba47af67de2c272a9be30af560b749c0
alanflpns <alanflpns@gmail.com>
2022-06-15 16:22:27 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,3 +1,5 @@
+import faker from "faker";
+
 import { RequiredFieldValidation } from "../../validators/required-field/required-field-validation";
 import { EmailValidation } from "../email/email-validation";
 import { MinLengthValidation } from "../min-length/min-length-validation";
@@ -5,17 +7,34 @@ import { ValidationBuilder } from "./validation-builder";
 
 describe("ValidationBuilder", () => {
   it("Should return RequiredFieldValidation", () => {
-    const validations = ValidationBuilder.field("any_field").required().build();
-    expect(validations).toEqual([new RequiredFieldValidation("any_field")]);
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field).required().build();
+    expect(validations).toEqual([new RequiredFieldValidation(field)]);
   });
 
   it("Should return EmailValidation", () => {
-    const validations = ValidationBuilder.field("any_field").email().build();
-    expect(validations).toEqual([new EmailValidation("any_field")]);
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field).email().build();
+    expect(validations).toEqual([new EmailValidation(field)]);
   });
 
   it("Should return MinLengthValidation", () => {
-    const validations = ValidationBuilder.field("any_field").min(5).build();
-    expect(validations).toEqual([new MinLengthValidation("any_field", 5)]);
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field).min(5).build();
+    expect(validations).toEqual([new MinLengthValidation(field, 5)]);
+  });
+
+  it("Should return a list of validations", () => {
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field)
+      .required()
+      .min(5)
+      .email()
+      .build();
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, 5),
+      new EmailValidation(field),
+    ]);
   });
 });
