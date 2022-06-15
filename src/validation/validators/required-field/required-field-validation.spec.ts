f6570c00b5f11e7e233bfdaaa6f75f4cc4ec0e76
alanflp <alanflpns@gmail.com>
2022-06-15 14:18:02 -0300

    test: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import faker from "faker";
+
+import { RequiredFieldError } from "../../errors";
+import { RequiredFieldValidation } from "./required-field-validation";
+
+const makeSut = () => new RequiredFieldValidation(faker.database.column());
+
+describe("RequiredFieldValidation", () => {
+  it("Should return error if field is empty", () => {
+    const sut = makeSut();
+    const error = sut.validate("");
+    expect(error).toEqual(new RequiredFieldError());
+  });
+
+  it("Should return falsy if field is not empty", () => {
+    const sut = makeSut();
+    const error = sut.validate(faker.random.word());
+    expect(error).toBeFalsy();
+  });
+});
