afe649452af751cdd710447bebaa905c43642e70
alanflpns <alanflpns@gmail.com>
2022-06-15 15:41:08 -0300

    feat: ensure validationComposite returns error if any validation fails

---
+++ src/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from "../../protocols/field-validation";
+
+export class FieldValidationSpy implements FieldValidation {
+  constructor(readonly field: string) {}
+  error: Error | null = null;
+
+  validate(value: string): Error | null {
+    return this.error;
+  }
+}
