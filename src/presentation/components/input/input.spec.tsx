acbe4d51005fe2b15e1224b029aa278de0de8f4c
alanflpns <alanflpns@gmail.com>
2022-06-20 14:17:12 -0300

    test: ensure input component readOnlu on focus

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,17 +1,31 @@
+/* eslint-disable testing-library/render-result-naming-convention */
 /* eslint-disable testing-library/prefer-screen-queries */
-import React from "react";
-import { render } from "@testing-library/react";
+import faker from "faker";
+import { fireEvent, render } from "@testing-library/react";
 import Input from "./input";
 import Context from "../../contexts/form/form-contex";
 
+const makeSut = (filedName: string) => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name={filedName} />
+    </Context.Provider>
+  );
+};
+
 describe("Input Component", () => {
   it("Shold begin with readOnly", () => {
-    const { getByTestId } = render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name="field" />
-      </Context.Provider>
-    );
-    const input = getByTestId("field") as HTMLInputElement;
+    const field = faker.database.column();
+    const sut = makeSut(field);
+    const input = sut.getByTestId("field") as HTMLInputElement;
     expect(input.readOnly).toBe(true);
+  });
+
+  it("Shold remove readOnly on focus", () => {
+    const field = faker.database.column();
+    const sut = makeSut(field);
+    const input = sut.getByTestId("field") as HTMLInputElement;
+    fireEvent.focus(input);
+    expect(input.readOnly).toBe(false);
   });
 });
