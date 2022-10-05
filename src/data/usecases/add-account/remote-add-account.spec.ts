172e21830cd93963f01f2b8901709aafc162eccb
alanflp <alanflpns@gmail.com>
2022-10-05 17:09:18 -0300

    feat: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,35 @@
+import faker from "faker";
+
+import { HttpPostClientSpy } from "../../test";
+import { RemoteAddAccount } from "./remote-add-account";
+import { mockAddAccountParams } from "../../../domain/test";
+import { AddAccountParams } from "../../../domain/usecases";
+import { AccountModel } from "../../../domain/models";
+
+type SutTypes = {
+  sut: RemoteAddAccount;
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>;
+};
+
+// Factory - Gerar o SUT
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AddAccountParams,
+    AccountModel
+  >();
+  const sut = new RemoteAddAccount(url, httpPostClientSpy);
+
+  return {
+    sut,
+    httpPostClientSpy,
+  };
+};
+
+describe("RemoteAddAccount", () => {
+  it("Should call HttpPostClient with correct URL", async () => {
+    const url = faker.internet.url();
+    const { sut, httpPostClientSpy } = makeSut(url);
+    await sut.add(mockAddAccountParams());
+    expect(httpPostClientSpy.url).toBe(url);
+  });
+});
---
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -0,0 +1,33 @@
+import {
+  InvalidCredentialsError,
+  UnexpectedError,
+} from "../../../domain/errors";
+import { AccountModel } from "../../../domain/models";
+import { AddAccount, AddAccountParams } from "../../../domain/usecases";
+import { HttpPostClient, HttpStatusCode } from "../../protocols/http";
+
+export class RemoteAddAccount implements AddAccount {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<
+      AddAccountParams,
+      AccountModel
+    >
+  ) {}
+
+  async add(params: AddAccountParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params,
+    });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body!;
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError();
+      default:
+        throw new UnexpectedError();
+    }
+  }
+}
