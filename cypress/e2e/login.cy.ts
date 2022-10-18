983aca1f6e36705110f36ba211d7a75a9419a70a
alanflp <alanflpns@gmail.com>
2022-10-18 16:05:29 -0300

    refactor: change test to simulate with enter key

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -122,9 +122,11 @@ describe("Login", () => {
     });
 
     cy.getByTestId("email").focus().type(faker.internet.email());
-    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(5));
+    cy.getByTestId("password")
+      .focus()
+      .type(faker.random.alphaNumeric(5))
+      .type("{enter}");
 
-    cy.getByTestId("submit").click();
     cy.getByTestId("main-error").should("not.exist");
     cy.getByTestId("spinner").should("not.exist");
 
@@ -132,5 +134,20 @@ describe("Login", () => {
     cy.window().then((window) =>
       assert.isOk(window.localStorage.getItem("accessToken"))
     );
+  });
+
+  it("Should prevent multiple submits", () => {
+    cy.intercept("POST", /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.random.uuid(),
+      },
+    }).as("request");
+
+    cy.getByTestId("email").focus().type(faker.internet.email());
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(5));
+
+    cy.getByTestId("submit").click();
+    cy.get("@request.all").should("have.length", 1);
   });
 });
