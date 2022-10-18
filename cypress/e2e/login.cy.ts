034002787290e47ac26dc8131a254b3a6abc3666
alanflp <alanflpns@gmail.com>
2022-10-18 15:55:05 -0300

    refactor: change test to use faker

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -101,8 +101,8 @@ describe("Login", () => {
       },
     });
 
-    cy.getByTestId("email").focus().type("mango@gmail.com");
-    cy.getByTestId("password").focus().type("12345");
+    cy.getByTestId("email").focus().type(faker.internet.email());
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(5));
 
     cy.getByTestId("submit").click();
     cy.getByTestId("spinner").should("not.exist");
@@ -121,8 +121,8 @@ describe("Login", () => {
       },
     });
 
-    cy.getByTestId("email").focus().type("mango@gmail.com");
-    cy.getByTestId("password").focus().type("12345");
+    cy.getByTestId("email").focus().type(faker.internet.email());
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(5));
 
     cy.getByTestId("submit").click();
     cy.getByTestId("main-error").should("not.exist");
