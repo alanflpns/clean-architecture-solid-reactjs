877e83265b4720a73ed9ea4ccebb70b5bd0e8819
alanflp <alanflpns@gmail.com>
2022-10-18 21:51:31 -0300

    refactor: improve input layout

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useRef } from "react";
 import styles from "./input-styles.module.scss";
 import Context from "../../contexts/form/form-contex";
-import { props } from "cypress/types/bluebird";
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -16,11 +15,16 @@ function Input({ ...rest }: Props) {
   const error = state[`${rest.name}Error`];
 
   return (
-    <div className={styles.inputWrap}>
+    <div
+      className={styles.inputWrap}
+      data-status={error ? "invalid" : "valid"}
+      data-testid={`${rest.name}-wrap`}
+    >
       <input
         {...rest}
         ref={inputRef}
         placeholder=" "
+        title={error || undefined}
         data-testid={rest.name}
         readOnly
         onFocus={(event) => (event.target.readOnly = false)}
@@ -28,16 +32,13 @@ function Input({ ...rest }: Props) {
           setState({ ...state, [event.target.name]: event.target.value })
         }
       />
-      <label onClick={() => inputRef.current?.focus()}>
+      <label
+        onClick={() => inputRef.current?.focus()}
+        title={error || undefined}
+        data-testid={`${rest.name}-label`}
+      >
         {rest.placeholder}
       </label>
-      <span
-        data-testid={`${rest.name}-status`}
-        title={error || "Tudo certo"}
-        className={styles.status}
-      >
-        {error ? "🔴" : "🟢"}
-      </span>
     </div>
   );
 }
